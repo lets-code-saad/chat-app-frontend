763e83230ab30f6122011b5c4ccaee8e428381f8
Saad <saadabbas594@gmail.com>
2025-06-12 22:32:19 +0500

    frontend interceptor change

--- src/Components/Middlewares/axiosInterceptor.js
+++ src/Components/Middlewares/axiosInterceptor.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const axiosInterceptor = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000", local host
+  baseURL: "https://chirpchat-backend.vercel.app", // live
 });
 
 axiosInterceptor.interceptors.request.use(
